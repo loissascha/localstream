cf907e0509fcc290a5872fdb0b8cb1ace330b08e
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-03-02 19:56:16 +0100

    fetch source show

--- backend/internal/entity/show.go
+++ backend/internal/entity/show.go
@@ -7,7 +7,24 @@ import (
 )
 
 type Show struct {
-	ID        uuid.UUID `db:"id"`
-	Name      string    `db:"name"`
-	CreatedAt time.Time `db:"created_at"`
+	ID          uuid.UUID   `db:"id"`
+	Name        string      `db:"name"`
+	CreatedAt   time.Time   `db:"created_at"`
+	FetchSource FetchSource `db:"fetch_source"`
+}
+
+type FetchSource string
+
+const (
+	FetchSourceTMDB   FetchSource = "tmdb"
+	FetchSourceTVMaze FetchSource = "tvmaze"
+)
+
+func (t FetchSource) IsValid() bool {
+	switch t {
+	case FetchSourceTMDB, FetchSourceTVMaze:
+		return true
+	default:
+		return false
+	}
 }
