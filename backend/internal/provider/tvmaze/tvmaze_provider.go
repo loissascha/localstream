3f09db495692950973ddc4010b71981cffc979db
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-03-02 19:29:48 +0100

    wip

--- backend/internal/provider/tvmaze/tvmaze_provider.go
+++ backend/internal/provider/tvmaze/tvmaze_provider.go
@@ -2,8 +2,9 @@ package tvmaze
 
 import (
 	"context"
+	"io"
 	"net/http"
-	"strings"
+	"net/url"
 	"time"
 
 	"github.com/loissascha/go-logger/logger"
@@ -24,15 +25,42 @@ func (p *TVMazeProvider) SearchSeries(episodeInfo *parsers.EpisodeInfo) {
 
 	logger.Info(nil, "Search series {Name}", episodeInfo)
 
-	var urlBuilder strings.Builder
-	urlBuilder.WriteString("https://api.tvmaze.com/search/shows?q=")
-	urlBuilder.WriteString(episodeInfo.Series)
-	url := urlBuilder.String()
+	params := url.Values{}
+	params.Add("q", episodeInfo.Series)
+	encoded := params.Encode()
 
-	logger.Info(nil, "URL: {Url}", url)
+	fullUrl := "https://api.tvmaze.com/search/shows?" + encoded
+
+	logger.Info(nil, "URL: {Url}", fullUrl)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	http.NewRequestWithContext()
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, fullUrl, nil)
+	if err != nil {
+		logger.Error(err, "Error with http request")
+		return
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		logger.Error(err, "Error with http request 2")
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		logger.Error(err, "Error with http request 3")
+		return
+	}
+
+	if resp.StatusCode != 200 {
+		logger.Error(nil, "Non success status code")
+		return
+	}
+
+	logger.Debug(nil, "Body: {Body}", string(body))
 }
