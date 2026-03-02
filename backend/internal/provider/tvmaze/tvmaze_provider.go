5dc0f0c697ad57a41895886c2e974a043a981b67
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-03-02 19:19:48 +0100

    tvmaze

--- backend/internal/provider/tvmaze/tvmaze_provider.go
+++ backend/internal/provider/tvmaze/tvmaze_provider.go
@@ -1,6 +1,8 @@
 package tvmaze
 
 import (
+	"strings"
+
 	"github.com/loissascha/go-logger/logger"
 	"github.com/loissascha/localstream/internal/parsers"
 )
@@ -18,4 +20,11 @@ func (p *TVMazeProvider) SearchSeries(episodeInfo *parsers.EpisodeInfo) {
 	}
 
 	logger.Info(nil, "Search series {Name}", episodeInfo)
+
+	var urlBuilder strings.Builder
+	urlBuilder.WriteString("https://api.tvmaze.com/search/shows?q=")
+	urlBuilder.WriteString(episodeInfo.Series)
+	url := urlBuilder.String()
+
+	logger.Info(nil, "URL: {Url}", url)
 }
