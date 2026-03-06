3a9970f8e6ef043843004260476e1e15e7a425cf
Sascha Loishandl <sascha@Saschas-MacBook-Air.local>
2026-03-06 13:36:47 +0100

    show handler

---
+++ backend/internal/handler/show_handler.go
@@ -0,0 +1,18 @@
+package handler
+
+import (
+	"github.com/loissascha/go-http-server/server"
+	"github.com/loissascha/localstream/internal/middleware"
+)
+
+type ShowHandler struct {
+	s              *server.Server
+	authMiddleware *middleware.AuthMiddleware
+}
+
+func NewShowHandler(s *server.Server, authMiddleware *middleware.AuthMiddleware) *ShowHandler {
+	return &ShowHandler{
+		s:              s,
+		authMiddleware: authMiddleware,
+	}
+}
