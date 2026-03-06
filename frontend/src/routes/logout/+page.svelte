4fd4d2fbba783d65a0258b62d187ed67dbd514e7
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-03-06 09:08:28 +0100

    logout page

---
+++ frontend/src/routes/logout/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { resolve } from '$app/paths';
+	import { clearAuth } from '$lib/auth.svelte';
+	import { deleteCookie } from '$lib/cookies';
+
+	$effect(() => {
+		deleteCookie('bearer');
+		clearAuth();
+
+		goto(resolve('/(auth)/login'));
+	});
+</script>
