308d8dd379f2cdb35524e47c770f1a532f4b674e
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-03-08 17:13:29 +0100

    loading shows

--- frontend/src/routes/(protected)/shows/[id]/+page.svelte
+++ frontend/src/routes/(protected)/shows/[id]/+page.svelte
@@ -1,4 +1,12 @@
 <script lang="ts">
+	let loadingShowData = $state(true);
+	let loadingSeasons = $state(true);
 </script>
 
-<main>page</main>
+<main class="min-h-dvh px-5 py-5">
+	{#if loadingShowData}
+		<p>Loading stuff...</p>
+	{:else}
+		<p>Show:</p>
+	{/if}
+</main>
