1689e556033c8a8d4dfa8b883088dc9ee58e1569
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-03-09 08:25:44 +0100

    show data

--- frontend/src/routes/(protected)/shows/[showID]/seasons/[seasonID]/episodes/[episodeID]/+page.svelte
+++ frontend/src/routes/(protected)/shows/[showID]/seasons/[seasonID]/episodes/[episodeID]/+page.svelte
@@ -2,10 +2,12 @@
 	import { page } from '$app/state';
 
 	const showId = $derived(page.params.showID ?? '');
+	const seasonId = $derived(page.params.seasonID ?? '');
 	const episodeId = $derived(page.params.episodeID ?? '');
 </script>
 
 <main>
 	Show: {showId} <br />
+	Season: {seasonId}<br />
 	Episode: {episodeId}
 </main>
