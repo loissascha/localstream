5ea6e7f0a6bc131250b499010ea95dc6fddde602
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-03-09 21:04:15 +0100

    export

--- frontend/src/lib/types/export_types.ts
+++ frontend/src/lib/types/export_types.ts
@@ -1,12 +1,3 @@
-export interface LibraryListItem {
-  id: string;
-  name: string;
-  path: string;
-  library_type: string;
-}
-export interface LibraryListResponse {
-  libraries: LibraryListItem[];
-}
 export interface AuthResponse {
   token: string;
 }
@@ -17,19 +8,26 @@ export interface ShowInfo {
 export interface ShowListResponse {
   shows: ShowInfo[];
 }
-export interface SeasonInfo {
-  id: string;
-  number: number;
-}
-export interface SeasonListResponse {
-  seasons: SeasonInfo[];
-}
 export interface EpisodeInfo {
   id: string;
   number: number;
 }
 export interface EpisodeListResponse {
   episodes: EpisodeInfo[];
+}
+export interface WatchstateResponse {
+  id: string;
+  show_id: string;
+  season_id: string;
+  episode_id: string;
+  position: number;
+  duration: number;
+  finished: boolean;
+  created_at: string;
+  updated_at: string;
+}
+export interface WatchstateListResponse {
+  watchstates: WatchstateResponse[];
 }
 export interface AuthUserResponse {
   id: number;
@@ -43,4 +41,28 @@ export interface VideoListItem {
   name: string;
   size: number;
   mimeType: string;
+}
+export interface LibraryListItem {
+  id: string;
+  name: string;
+  path: string;
+  library_type: string;
+}
+export interface LibraryListResponse {
+  libraries: LibraryListItem[];
+}
+export interface SeasonInfo {
+  id: string;
+  number: number;
+}
+export interface SeasonListResponse {
+  seasons: SeasonInfo[];
+}
+export interface SaveWatchstateRequest {
+  show_id: string;
+  season_id: string;
+  episode_id: string;
+  position: number;
+  duration: number;
+  finished: boolean;
 }
