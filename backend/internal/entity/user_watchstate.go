8c2e42c81d24f6fc4d7f0b2ea2e05b65aebaa1eb
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-03-09 20:48:22 +0100

    user watchstate

---
+++ backend/internal/entity/user_watchstate.go
@@ -0,0 +1,19 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type UserWatchstate struct {
+	ID        uuid.UUID `db:"id"`
+	ShowID    uuid.UUID `db:"show_id"`
+	SeasonID  uuid.UUID `db:"season_id"`
+	EpisodeID uuid.UUID `db:"episode_id"`
+	Position  float64   `db:"position"`
+	Duration  float64   `db:"duration"`
+	Finished  bool      `db:"finished"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
+}
