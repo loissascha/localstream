f760ac26d531ef13cb4f61a106b88c6b92864e1f
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-03-28 10:53:12 +0100

    admin list libraries

--- frontend/src/routes/(protected)/(admin)/admin/libraries/+page.svelte
+++ frontend/src/routes/(protected)/(admin)/admin/libraries/+page.svelte
@@ -1,1 +1,28 @@
-Libraries Page
+<script lang="ts">
+	import { loadLibraries } from '$lib/api/libraries';
+	import { auth } from '$lib/auth.svelte';
+	import type { LibraryListItem } from '$lib/types/export_types';
+
+	let libraries = $state<LibraryListItem[]>([]);
+
+	async function loadLibs() {
+		if (!auth.token) {
+			throw new Error('no auth token');
+		}
+		const data = await loadLibraries(auth.token);
+		libraries = data.libraries;
+	}
+
+	$effect(() => {
+		if (!auth.token) return;
+		loadLibs();
+	});
+</script>
+
+<div>
+	{#each libraries as library}
+		<div>
+			{library.name}
+		</div>
+	{/each}
+</div>
