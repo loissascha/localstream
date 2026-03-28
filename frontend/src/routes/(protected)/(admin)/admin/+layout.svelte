8cc1c0339f156aa6ff973dd096ffac7d59095e14
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-03-28 10:13:57 +0100

    layout

--- frontend/src/routes/(protected)/(admin)/admin/+layout.svelte
+++ frontend/src/routes/(protected)/(admin)/admin/+layout.svelte
@@ -13,22 +13,188 @@
 	});
 </script>
 
-<section id="header" class="flex items-center justify-between bg-neutral-900 px-4 py-4">
-	<div>
-		<a href={resolve('/(protected)/(user)')} class="text-2xl font-semibold tracking-wider"
-			>Localstream Admin</a
-		>
+<div class="flex h-dvh flex-col">
+	<section id="header" class="flex items-center justify-between bg-neutral-900 px-4 py-4">
+		<div>
+			<a href={resolve('/(protected)/(user)')} class="text-2xl font-semibold tracking-wider"
+				>Localstream Admin</a
+			>
+		</div>
+		<div>
+			<a
+				href={resolve('/logout')}
+				type="submit"
+				class="cursor-pointer rounded-md border border-neutral-500 bg-neutral-600 px-3 py-1.5 text-sm hover:bg-neutral-500"
+			>
+				Log out
+			</a>
+		</div>
+	</section>
+	<div class="flex grow">
+		<section id="left" class="w-80 shrink-0 grow-0 overflow-y-auto bg-neutral-800">
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+			Left <br />
+		</section>
+		<section id="content" class="grow overflow-y-auto p-4">
+			{@render children()}
+		</section>
 	</div>
-	<div>
-		<a
-			href={resolve('/logout')}
-			type="submit"
-			class="cursor-pointer rounded-md border border-neutral-500 bg-neutral-600 px-3 py-1.5 text-sm hover:bg-neutral-500"
-		>
-			Log out
-		</a>
-	</div>
-</section>
-<section id="content" class="p-4">
-	{@render children()}
-</section>
+</div>
