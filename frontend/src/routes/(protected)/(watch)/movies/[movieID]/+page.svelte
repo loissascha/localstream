a2866280d807211739dadabb7cf013f86069c1d7
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-03-31 20:33:38 +0200

    comment

--- frontend/src/routes/(protected)/(watch)/movies/[movieID]/+page.svelte
+++ frontend/src/routes/(protected)/(watch)/movies/[movieID]/+page.svelte
@@ -17,52 +17,52 @@
 	);
 	// let logTimer: ReturnType<typeof setInterval> | null = null;
 
-	 // function stopPlaybackLogging() {
-	 // 	if (logTimer !== null) {
-	 // 		clearInterval(logTimer);
-	 // 		logTimer = null;
-	 // 	}
-	 // }
-	
-	 // async function logPlaybackStatus() {
-	 // 	if (!videoEl) {
-	 // 		return;
-	 // 	}
-	 // 	if (loadingWatchstate) {
-	 // 		return;
-	 // 	}
-	 //
-	 // 	const duration = Number.isFinite(videoEl.duration) ? Number(videoEl.duration.toFixed(2)) : 0;
-	 // 	const position = Number(videoEl.currentTime.toFixed(2));
-	 // 	const finished = duration > 0 && position >= Math.max(duration - 10, 0);
-	 // 	// almostDone = duration > 0 && position >= Math.max(duration - 120, 0);
-	 //
-	 // 	if (auth.token) {
-	 // 		try {
-	 // 		console.log("log watchsatte")
-	 // 		} catch (e) {
-	 // 			console.error(e);
-	 // 		}
-	 // 	}
-	 // }
-	 //
-	 // function startPlaybackLogging() {
-	 // 	stopPlaybackLogging();
-	 // 	logPlaybackStatus();
-	 // 	logTimer = setInterval(logPlaybackStatus, 5000);
-	 //
-	 //
-	 // onDestroy(() => {
-	 // 	stopPlaybackLogging();
-	 // });
-	 //
-	 // $effect(() => {
-	 // 	if (!auth.token) {
-	 // 		return;
-	 // 	}
-	 // 	if (videoEl == null) return;
-	 // 	loadingWatchstate = false;
-	 // });
+	// function stopPlaybackLogging() {
+	// 	if (logTimer !== null) {
+	// 		clearInterval(logTimer);
+	// 		logTimer = null;
+	// 	}
+	// }
+
+	// async function logPlaybackStatus() {
+	// 	if (!videoEl) {
+	// 		return;
+	// 	}
+	// 	if (loadingWatchstate) {
+	// 		return;
+	// 	}
+	//
+	// 	const duration = Number.isFinite(videoEl.duration) ? Number(videoEl.duration.toFixed(2)) : 0;
+	// 	const position = Number(videoEl.currentTime.toFixed(2));
+	// 	const finished = duration > 0 && position >= Math.max(duration - 10, 0);
+	// 	// almostDone = duration > 0 && position >= Math.max(duration - 120, 0);
+	//
+	// 	if (auth.token) {
+	// 		try {
+	// 		console.log("log watchsatte")
+	// 		} catch (e) {
+	// 			console.error(e);
+	// 		}
+	// 	}
+	// }
+	//
+	// function startPlaybackLogging() {
+	// 	stopPlaybackLogging();
+	// 	logPlaybackStatus();
+	// 	logTimer = setInterval(logPlaybackStatus, 5000);
+	//
+	//
+	// onDestroy(() => {
+	// 	stopPlaybackLogging();
+	// });
+	//
+	// $effect(() => {
+	// 	if (!auth.token) {
+	// 		return;
+	// 	}
+	// 	if (videoEl == null) return;
+	// 	loadingWatchstate = false;
+	// });
 </script>
 
 <main class="grid min-h-dvh grid-rows-[auto_1fr]">
@@ -80,9 +80,6 @@
 			controls
 			preload="metadata"
 			src={streamUrl}
-			onplay={startPlaybackLogging}
-			onpause={stopPlaybackLogging}
-			onended={stopPlaybackLogging}
 		></video>
 	</section>
 </main>
