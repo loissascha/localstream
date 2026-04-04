b169fcde34f30601c158a8c553a246f0a3469ff4
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-04-04 07:46:30 +0200

    error

--- backend/internal/entity/fetch_source.go
+++ backend/internal/entity/fetch_source.go
@@ -4,15 +4,15 @@ type FetchSource string
 
 const (
 	FetchSourceNone   FetchSource = "none"
+	FetchSourceError  FetchSource = "error"
 	FetchSourceTMDB   FetchSource = "tmdb"
 	FetchSourceTVMaze FetchSource = "tvmaze"
 )
 
-func (t FetchSource) IsValid() bool {
-	switch t {
-	case FetchSourceTMDB, FetchSourceTVMaze:
-		return true
-	default:
-		return false
-	}
+func (t FetchSource) IsNone() bool {
+	return t == FetchSourceNone
+}
+
+func (t FetchSource) IsError() bool {
+	return t == FetchSourceError
 }
