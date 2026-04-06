ef6a1235a46a0d4ec5f1655abbc9a85bc0066ccd
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-04-06 17:21:48 +0200

    admin show metadata block

---
+++ frontend/src/lib/components/AdminShowMetadataBlock.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { ShowInfo } from '$lib/types/export_types';
+
+	let { show }: { show: ShowInfo } = $props();
+</script>
+
+<div>
+	{show.name}
+</div>
--- frontend/src/routes/(protected)/(admin)/admin/metadata/+page.svelte
+++ frontend/src/routes/(protected)/(admin)/admin/metadata/+page.svelte
@@ -1,4 +1,35 @@
 <script lang="ts">
+	import { loadShows } from '$lib/api/shows';
+	import { auth } from '$lib/auth.svelte';
+	import AdminShowMetadataBlock from '$lib/components/AdminShowMetadataBlock.svelte';
+	import type { ShowInfo } from '$lib/types/export_types';
+
+	var shows = $state<ShowInfo[]>([]);
+	var loadingShows = $state(true);
+	var errorMessage = $state('');
+
+	async function loadShowsList() {
+		try {
+			if (!auth.token) return;
+			const data = await loadShows(auth.token);
+			shows = data.shows;
+		} catch (e) {
+			errorMessage = (e as Error).message;
+		} finally {
+			loadingShows = true;
+		}
+	}
+
+	$effect(() => {
+		if (!auth.initialized) return;
+		loadShowsList();
+	});
 </script>
 
-Metadata page
+{#if errorMessage != ''}
+	<p class="text-red-500">{errorMessage}</p>
+{/if}
+
+{#each shows as show}
+	<AdminShowMetadataBlock {show} />
+{/each}
