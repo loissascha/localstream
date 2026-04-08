55a0be6abd913a1a7a6d5001c6f3cfd9495b84c2
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-04-08 16:03:56 +0200

    refactor get next episode

--- backend/internal/service/episode_service.go
+++ backend/internal/service/episode_service.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/google/uuid"
 	"github.com/loissascha/localstream/internal/encoders"
 	"github.com/loissascha/localstream/internal/entity"
 	"github.com/loissascha/localstream/internal/repository"
@@ -11,10 +12,11 @@ import (
 
 type EpisodeService struct {
 	episodeRepo repository.EpisodeRepository
+	seasonRepo  repository.SeasonRepository
 }
 
-func NewEpisodeService(episodeRepo repository.EpisodeRepository) *EpisodeService {
-	return &EpisodeService{episodeRepo: episodeRepo}
+func NewEpisodeService(episodeRepo repository.EpisodeRepository, seasonRepo repository.SeasonRepository) *EpisodeService {
+	return &EpisodeService{episodeRepo: episodeRepo, seasonRepo: seasonRepo}
 }
 
 func (s *EpisodeService) ListBySeasonID(ctx context.Context, seasonId string) ([]entity.Episode, error) {
@@ -40,6 +42,15 @@ func (s *EpisodeService) GetByID(ctx context.Context, episodeId string) (*entity
 	return s.episodeRepo.GetByID(ctx, id)
 }
 
+func (s *EpisodeService) getShowIdForSeasonId(ctx context.Context, seasonId uuid.UUID) (uuid.UUID, error) {
+	season, err := s.seasonRepo.GetByID(ctx, seasonId)
+	if err != nil {
+		return uuid.Nil, err
+	}
+	showId := season.ShowID
+	return showId, nil
+}
+
 func (s *EpisodeService) GetNextEpisode(ctx context.Context, episodeId string) (*entity.Episode, error) {
 	id, err := encoders.DecodeUUID(episodeId)
 	if err != nil {
@@ -53,12 +64,17 @@ func (s *EpisodeService) GetNextEpisode(ctx context.Context, episodeId string) (
 
 	number := episode.Number
 	seasonId := episode.SeasonID
+	showId, err := s.getShowIdForSeasonId(ctx, seasonId)
+	if err != nil {
+		return nil, err
+	}
 
 	episodes, err := s.episodeRepo.ListBySeasonID(ctx, seasonId)
 	if err != nil {
 		return nil, err
 	}
 
+	// if there is a next episode within the current season -> return it
 	useNext := false
 	for _, e := range episodes {
 		if e.Number == number {
@@ -70,6 +86,40 @@ func (s *EpisodeService) GetNextEpisode(ctx context.Context, episodeId string) (
 		}
 	}
 
-	// TODO: check if there is a next season and get first episode of that season
+	// if there is a next season -> take the first episode of it
+	nextSeasonId, err := s.getNextSeasonId(ctx, seasonId, showId)
+	if err != nil {
+		return nil, err
+	}
+	if nextSeasonId == uuid.Nil {
+		return nil, nil
+	}
+	episodes, err = s.episodeRepo.ListBySeasonID(ctx, nextSeasonId)
+	if err != nil {
+		return nil, err
+	}
+	if len(episodes) > 0 {
+		return &episodes[0], nil
+	}
+
 	return nil, nil
+}
+
+func (s *EpisodeService) getNextSeasonId(ctx context.Context, seasonId, showId uuid.UUID) (uuid.UUID, error) {
+	seasons, err := s.seasonRepo.ListByShowID(ctx, showId)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	useNext := false
+	for _, se := range seasons {
+		if se.ID == seasonId {
+			useNext = true
+			continue
+		}
+		if useNext {
+			return se.ID, nil
+		}
+	}
+	return uuid.Nil, nil
 }
