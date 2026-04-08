2b1e2d5aa11692b6943b1fa61baf4ab874e1c495
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-04-08 15:46:23 +0200

    layout

--- frontend/src/routes/(protected)/(user)/+layout.svelte
+++ frontend/src/routes/(protected)/(user)/+layout.svelte
@@ -6,10 +6,20 @@
 </script>
 
 <section id="header" class="flex items-center justify-between px-4 py-4">
-	<div>
+	<div class="flex items-center gap-2">
 		<a href={resolve('/(protected)/(user)')} class="text-2xl font-semibold tracking-wider"
 			>Localstream</a
 		>
+		<a
+			class="cursor-pointer rounded-md border border-neutral-500 bg-neutral-600 px-3 py-1.5 text-sm hover:bg-neutral-500"
+		>
+			Shows
+		</a>
+		<a
+			class="cursor-pointer rounded-md border border-neutral-500 bg-neutral-600 px-3 py-1.5 text-sm hover:bg-neutral-500"
+		>
+			Movies
+		</a>
 	</div>
 	<div class="flex items-center gap-2">
 		{#if auth.isAdmin}
