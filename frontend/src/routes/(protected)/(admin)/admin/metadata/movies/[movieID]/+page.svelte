376b68ee105ea36d8878283d748746dab70599d5
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-04-14 14:50:30 +0200

    movie details

--- frontend/src/routes/(protected)/(admin)/admin/metadata/movies/[movieID]/+page.svelte
+++ frontend/src/routes/(protected)/(admin)/admin/metadata/movies/[movieID]/+page.svelte
@@ -1,14 +1,29 @@
 <script lang="ts">
 	import { page } from '$app/state';
 	import { loadMovieMetadata } from '$lib/api/movie_metadata';
+	import { getMovie } from '$lib/api/movies';
 	import { auth } from '$lib/auth.svelte';
 	import SearchIcon from '$lib/icons/SearchIcon.svelte';
-	import type { MovieMetadataInfo } from '$lib/types/export_types';
+	import { type MovieInfo, type MovieMetadataInfo } from '$lib/types/export_types';
 
 	const movieID = $derived(page.params.movieID ?? '');
 
 	let metadata = $state<MovieMetadataInfo[]>([]);
+	let movie = $state<MovieInfo | null>(null);
+	let loadingMovie = $state(true);
 	let loadingMetadata = $state(true);
+
+	async function loadMovieData() {
+		try {
+			if (!auth.token) return;
+			movie = await getMovie(auth.token, movieID);
+		} catch (e) {
+			const m = (e as Error).message;
+			alert(m);
+		} finally {
+			loadingMovie = false;
+		}
+	}
 
 	async function loadMetadata() {
 		try {
@@ -27,24 +42,35 @@
 		if (!auth.token) return;
 		if (!auth.isAdmin) return;
 		movieID;
+		loadMovieData();
 		loadMetadata();
 	});
+
+	async function submitMetadataSearchForm() {}
 </script>
 
-Details Movie {movieID}
+<h1 class="text-2xl font-bold">{movie?.name}</h1>
 
 {#if loadingMetadata}
 	Loading...
 {:else if metadata.length == 0}
 	<section id="metadata-search" class="my-8">
-		<h2 class="text-2xl font-bold">Search Metadata</h2>
-		<form class="flex items-center gap-2">
+		<h2 class="text-xl font-bold">Search Metadata</h2>
+		<form
+			onsubmit={(e) => {
+				e.preventDefault();
+				submitMetadataSearchForm();
+			}}
+			class="flex items-center gap-2"
+		>
 			<input
 				type="text"
 				class="my-4 w-full rounded bg-neutral-800 px-4 py-2"
 				placeholder="Search by Name"
 			/>
-			<button class="flex cursor-pointer gap-2 rounded bg-neutral-700 hover:bg-neutral-600 px-4 py-2">
+			<button
+				class="flex cursor-pointer gap-2 rounded bg-neutral-700 px-4 py-2 hover:bg-neutral-600"
+			>
 				<SearchIcon /> Search
 			</button>
 		</form>
