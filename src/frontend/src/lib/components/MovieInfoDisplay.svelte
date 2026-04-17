41375790bc469da06834f948cd657b6f0f2102e0
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-04-17 09:21:36 +0200

    dont fetch metadata for info displays

--- src/frontend/src/lib/components/MovieInfoDisplay.svelte
+++ src/frontend/src/lib/components/MovieInfoDisplay.svelte
@@ -1,36 +1,12 @@
 <script lang="ts">
-	import { loadMovieMetadata } from '$lib/api/movie_metadata';
-	import { auth } from '$lib/auth.svelte';
-	import { type MovieMetadataInfo, type MovieInfo } from '$lib/types/export_types';
+	import { type MovieInfo } from '$lib/types/export_types';
 
 	let { movie }: { movie: MovieInfo } = $props();
-
-	let metadata = $state<MovieMetadataInfo | null>(null);
-
-	async function loadMetadata() {
-		try {
-			if (!auth.token) return;
-			const mlist = await loadMovieMetadata(auth.token, movie.id);
-			if (mlist.length == 1) {
-				metadata = mlist[0];
-			}
-		} catch (e) {
-			const m = (e as Error).message;
-			alert(m);
-		}
-	}
-
-	$effect(() => {
-		if (!auth.initialized) return;
-		loadMetadata();
-	});
 </script>
 
-{#if metadata == null || metadata.medium_image_url == ''}
-	<span class="font-bold">{movie.name}</span>
-{:else}
-	<div>
-		<img alt={movie.name} class="rounded w-full" src={metadata.medium_image_url} />
-		<div class="my-1 text-center font-bold">{movie.name}</div>
-	</div>
-{/if}
+<div>
+	{#if movie.medium_image_url != ''}
+		<img alt={movie.name} class="w-full rounded" src={movie.medium_image_url} />
+	{/if}
+	<div class="my-1 text-center font-bold">{movie.name}</div>
+</div>
--- src/frontend/src/lib/components/ShowInfoDisplay.svelte
+++ src/frontend/src/lib/components/ShowInfoDisplay.svelte
@@ -1,36 +1,12 @@
 <script lang="ts">
-	import { loadShowMetadata } from '$lib/api/show_metadata';
-	import { auth } from '$lib/auth.svelte';
-	import { type ShowMetadataInfo, type ShowInfo } from '$lib/types/export_types';
+	import { type ShowInfo } from '$lib/types/export_types';
 
 	let { show }: { show: ShowInfo } = $props();
-
-	let metadata = $state<ShowMetadataInfo | null>(null);
-
-	async function loadMetadata() {
-		try {
-			if (!auth.token) return;
-			const mlist = await loadShowMetadata(auth.token, show.id);
-			if (mlist.length == 1) {
-				metadata = mlist[0];
-			}
-		} catch (e) {
-			const m = (e as Error).message;
-			alert(m);
-		}
-	}
-
-	$effect(() => {
-		if (!auth.initialized) return;
-		loadMetadata();
-	});
 </script>
 
-{#if metadata == null || metadata.medium_image_url == ''}
-	<span class="font-bold">{show.name}</span>
-{:else}
-	<div>
-		<img alt={show.name} class="rounded w-full" src={metadata.medium_image_url} />
-		<div class="text-center font-bold my-1">{show.name}</div>
-	</div>
-{/if}
+<div>
+	{#if show.medium_image_url != ''}
+		<img alt={show.name} class="w-full rounded" src={show.medium_image_url} />
+	{/if}
+	<div class="my-1 text-center font-bold">{show.name}</div>
+</div>
