296817f4bb4d3f8fc4df2124afac739ca917e4ea
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-04-18 18:40:02 +0200

    selectable movies -> group action to add to collection

--- src/frontend/src/routes/(protected)/(user)/movies/+page.svelte
+++ src/frontend/src/routes/(protected)/(user)/movies/+page.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
+	import { addMovieToCollection } from '$lib/api/collections';
 	import { listMovies } from '$lib/api/movies';
 	import { auth } from '$lib/auth.svelte';
 	import ItemGrid from '$lib/components/ItemGrid.svelte';
 	import MovieListItem from '$lib/components/MovieListItem.svelte';
+	import SelectCollectionOverlay from '$lib/components/overlays/SelectCollectionOverlay.svelte';
 	import type { MovieInfo } from '$lib/types/export_types';
 
 	let movies = $state<MovieInfo[]>([]);
 	let selectedMovies = $state<Record<string, boolean>>({});
+	let selectedMoviesCount = $state(0);
 	let loading = $state(true);
+	let showAddToCollection = $state(false);
 
 	async function loadMovies() {
 		try {
@@ -30,9 +34,36 @@
 		}
 	}
 
+	async function addSelectedMoviesToCollection(collectionId: string) {
+		try {
+			if (!auth.token) return;
+			for (const [id, isSelected] of Object.entries(selectedMovies)) {
+				if (isSelected) {
+					await addMovieToCollection(auth.token, collectionId, id);
+				}
+			}
+			selectedMovies = Object.fromEntries(movies.map((movie) => [movie.id, false]));
+		} catch (e) {
+			alert((e as Error).message);
+		} finally {
+			showAddToCollection = false;
+		}
+	}
+
 	$effect(() => {
 		if (!auth.initialized) return;
 		loadMovies();
+	});
+
+	$effect(() => {
+		selectedMovies;
+		var sc = 0;
+		for (const [id, isSelected] of Object.entries(selectedMovies)) {
+			if (isSelected) {
+				sc++;
+			}
+		}
+		selectedMoviesCount = sc;
 	});
 </script>
 
@@ -40,6 +71,17 @@
 	{#if loading}
 		<p>Loading movies...</p>
 	{:else}
+		{#if selectedMoviesCount > 0}
+			<section class="flex items-center justify-end gap-4">
+				<button
+					class="cursor-pointer rounded-full bg-neutral-800 px-4 py-2 hover:bg-neutral-700"
+					onclick={() => {
+						showAddToCollection = true;
+					}}>Add all to Collection</button
+				>
+				<div>{selectedMoviesCount} selected</div>
+			</section>
+		{/if}
 		<section class="my-8">
 			<ItemGrid>
 				{#each movies as movie (movie.id)}
@@ -49,3 +91,14 @@
 		</section>
 	{/if}
 </main>
+
+{#if showAddToCollection}
+	<SelectCollectionOverlay
+		selectedCollection={(collectionId) => {
+			addSelectedMoviesToCollection(collectionId);
+		}}
+		close={() => {
+			showAddToCollection = false;
+		}}
+	/>
+{/if}
