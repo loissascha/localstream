b5b02b890873e1bbfa1dc620f97b0ef92b20b63b
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-04-18 10:41:35 +0200

    collections page

--- src/frontend/src/routes/(protected)/(user)/collections/+page.svelte
+++ src/frontend/src/routes/(protected)/(user)/collections/+page.svelte
@@ -1,4 +1,32 @@
 <script lang="ts">
+	import { listCollections } from '$lib/api/collections';
+	import { auth } from '$lib/auth.svelte';
+	import PlusIcon from '$lib/icons/PlusIcon.svelte';
+	import type { CollectionInfo } from '$lib/types/export_types';
+
+	let collections = $state<CollectionInfo[]>([]);
+
+	async function loadData() {
+		if (!auth.token) return;
+		const result = await listCollections(auth.token);
+		collections = result.collections;
+	}
+
+	$effect(() => {
+		if (!auth.initialized) return;
+		if (!auth.token) return;
+		loadData();
+	});
 </script>
 
-Collections
+<section class="flex items-center justify-center gap-4">
+	<div>
+		{collections.length} Collections
+	</div>
+	<button
+		class="flex cursor-pointer gap-1 rounded-full bg-neutral-800 px-4 py-2 hover:bg-neutral-700"
+	>
+		<PlusIcon />
+		Create Collection
+	</button>
+</section>
