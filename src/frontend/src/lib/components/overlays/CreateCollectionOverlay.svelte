e862e2f8f970d4e2791d680d6a7952e870fbc7d0
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-04-18 11:38:15 +0200

    create collection

--- src/frontend/src/lib/components/overlays/CreateCollectionOverlay.svelte
+++ src/frontend/src/lib/components/overlays/CreateCollectionOverlay.svelte
@@ -5,13 +5,36 @@
 		close: () => void;
 	}
 	let { close }: Props = $props();
+
+	let name = $state('');
+	let error_message = $state('');
+
+	function submitForm() {
+		if (name.trim() == '') {
+			error_message = 'Name must not be empty!';
+			return;
+		}
+		name = '';
+	}
 </script>
 
 <Overlay {close}>
 	<h1 class="text-2xl font-bold tracking-wide">Create Collection</h1>
-	<form class="mt-8 mb-4">
+	{#if error_message != ''}
+		<div class="mt-4 text-red-500">
+			{error_message}
+		</div>
+	{/if}
+	<form
+		class="mt-4 mb-4"
+		onsubmit={(e) => {
+			e.preventDefault();
+			submitForm();
+		}}
+	>
 		<label for="name">Collection Name</label>
 		<input
+			bind:value={name}
 			id="name"
 			type="text"
 			class="w-full rounded bg-neutral-700 px-4 py-2"
