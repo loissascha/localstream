514749a05b9b0ef989f37a7f217dd7a4cf87def0
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-04-25 09:40:31 +0200

    refactor

--- src/frontend/src/lib/components/ui/DropdownMelt.svelte
+++ src/frontend/src/lib/components/ui/DropdownMelt.svelte
@@ -5,13 +5,14 @@
 	interface Props {
 		children: Snippet;
 		items: Snippet;
+		className?: string;
 	}
-	let { children, items }: Props = $props();
+	let { children, items, className = '' }: Props = $props();
 
 	const popover = new Popover();
 </script>
 
-<button {...popover.trigger} class="cursor-pointer">{@render children()}</button>
+<button {...popover.trigger} class={`cursor-pointer ${className}`}>{@render children()}</button>
 <div {...popover.content} class="rounded-md bg-neutral-800 text-white shadow-lg">
 	<div {...popover.arrow}></div>
 	{@render items()}
--- src/frontend/src/routes/(protected)/(admin)/admin/+layout.svelte
+++ src/frontend/src/routes/(protected)/(admin)/admin/+layout.svelte
@@ -4,6 +4,9 @@
 	import AdminLeftMenuButton from '$lib/components/admin/AdminLeftMenuButton.svelte';
 	import Logo from '$lib/components/ui/Logo.svelte';
 	import LayoutUserDropdown from '$lib/components/ui/LayoutUserDropdown.svelte';
+	import MenuIcon from '$lib/icons/MenuIcon.svelte';
+	import DropdownMelt from '$lib/components/ui/DropdownMelt.svelte';
+	import DropdownItem from '$lib/components/ui/DropdownItem.svelte';
 
 	let { children } = $props();
 
@@ -22,15 +25,36 @@
 				class="flex items-center gap-2 text-2xl font-semibold tracking-wider select-none"
 				><Logo />ocalstream</a
 			>
+			<DropdownMelt className="lg:hidden">
+				<MenuIcon />
+				{#snippet items()}
+					<DropdownItem href={resolve('/(protected)/(user)')}>
+						<div class="flex items-center gap-1">Home</div>
+					</DropdownItem>
+					{#if auth.isAdmin}
+						<div class="flex flex-col gap-2">
+							<span class="px-2 tracking-wide text-neutral-300 uppercase">Admin</span>
+							<DropdownItem href={resolve('/(protected)/(admin)/admin')}>Dashboard</DropdownItem>
+							<DropdownItem href={resolve('/(protected)/(admin)/admin/libraries')}
+								>Libraries</DropdownItem
+							>
+							<DropdownItem href={resolve('/(protected)/(admin)/admin/users')}>Users</DropdownItem>
+							<DropdownItem href={resolve('/(protected)/(admin)/admin/metadata')}
+								>Metadata</DropdownItem
+							>
+						</div>
+					{/if}
+				{/snippet}
+			</DropdownMelt>
 		</div>
 		<div class="flex items-center gap-2">
 			<LayoutUserDropdown />
 		</div>
 	</section>
-	<div class="flex min-h-0 grow">
+	<div class="block min-h-0 grow lg:flex">
 		<section
 			id="left"
-			class="w-80 shrink-0 grow-0 overflow-y-auto border-r border-r-neutral-600 p-4"
+			class="hidden w-80 shrink-0 grow-0 overflow-y-auto border-r border-r-neutral-600 p-4 lg:block"
 		>
 			<div class="mb-4 flex flex-col gap-2 border-b border-neutral-600 pb-4">
 				<AdminLeftMenuButton href={resolve('/(protected)/(user)')}>Home</AdminLeftMenuButton>
