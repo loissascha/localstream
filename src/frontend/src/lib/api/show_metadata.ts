6205aa12c43f1bd020b5d2267793eff337e03337
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-05-02 19:18:58 +0200

    search show metadata api

--- src/frontend/src/lib/api/show_metadata.ts
+++ src/frontend/src/lib/api/show_metadata.ts
@@ -1,4 +1,4 @@
-import type { ShowMetadataInfo } from '$lib/types/export_types';
+import type { ShowMetadataInfo, ShowSearchResult } from '$lib/types/export_types';
 
 export async function loadShowMetadata(
 	bearerToken: string,
@@ -18,5 +18,24 @@ export async function loadShowMetadata(
 		return data;
 	} catch (error) {
 		throw error;
+	}
+}
+
+export async function searchShowMetadata(bearerToken: string, searchQuery: string) {
+	try {
+		const res = await fetch('/api/v1/show/metadata/search?q=' + encodeURIComponent(searchQuery), {
+			method: 'POST',
+			headers: {
+				Authorization: 'Bearer ' + bearerToken
+			}
+		});
+		if (!res.ok) {
+			throw new Error(`Failed to load metadata: ${res.status}`);
+		}
+
+		const data = (await res.json()) as ShowSearchResult[];
+		return data;
+	} catch (e) {
+		throw e;
 	}
 }
