4f1a1fa96fe792064d94f05c9acc46c2da4ce1ed
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-05-03 18:28:10 +0200

    last watched overlay

--- src/frontend/src/lib/components/LastWatched.svelte
+++ src/frontend/src/lib/components/LastWatched.svelte
@@ -4,12 +4,17 @@
 	import { listLatestWatchstateByShow } from '$lib/api/watchstate';
 	import { auth } from '$lib/auth.svelte';
 	import ChevronRightIcon from '$lib/icons/ChevronRightIcon.svelte';
-	import { type WatchstateResponse } from '$lib/types/export_types';
+	import type { ShowInfo, WatchstateResponse } from '$lib/types/export_types';
 	import ItemGrid from './ItemGrid.svelte';
 	import ListItemA from './ListItemA.svelte';
+	import ShowMetadataSearchOverlay from './overlays/ShowMetadataSearchOverlay.svelte';
 	import ShowInfoDisplay from './ShowInfoDisplay.svelte';
+	import ContextMenu from './ui/ContextMenu.svelte';
 
 	let data = $state<WatchstateResponse[]>([]);
+
+	let showMetadataOverlayOpen = $state(false);
+	let showMetadataShow = $state<ShowInfo | null>(null);
 
 	async function updateData() {
 		if (!auth.token) {
@@ -43,23 +48,52 @@
 		<ItemGrid>
 			{#each data as d (d.id)}
 				{#if !d.finished}
-					<ListItemA
-						href={resolve(
-							'/(protected)/watch/shows/[showID]/seasons/[seasonID]/episodes/[episodeID]',
-							{
-								showID: d.show_id,
-								seasonID: d.season_id,
-								episodeID: d.episode_id
-							}
-						)}
-					>
-						<ShowInfoDisplay show={d.show_info} nameLink percentage={d.percentage} showPercentage />
-						<div>
-							<div>S{d.season_info.number}:E{d.episode_info.number}</div>
-						</div>
-					</ListItemA>
+					<ContextMenu closeOnItemClick={true}>
+						<ListItemA
+							href={resolve(
+								'/(protected)/watch/shows/[showID]/seasons/[seasonID]/episodes/[episodeID]',
+								{
+									showID: d.show_id,
+									seasonID: d.season_id,
+									episodeID: d.episode_id
+								}
+							)}
+						>
+							<ShowInfoDisplay
+								show={d.show_info}
+								nameLink
+								percentage={d.percentage}
+								showPercentage
+							/>
+							<div>
+								<div>S{d.season_info.number}:E{d.episode_info.number}</div>
+							</div>
+						</ListItemA>
+						{#snippet items(closeMenu)}
+							<button
+								role="menuitem"
+								onclick={(e) => {
+									e.preventDefault();
+									e.stopPropagation();
+									closeMenu();
+									showMetadataOverlayOpen = true;
+									showMetadataShow = d.show_info;
+								}}
+								class="cursor-pointer px-4 py-2 hover:bg-neutral-700">Update Metadata</button
+							>
+						{/snippet}
+					</ContextMenu>
 				{/if}
 			{/each}
 		</ItemGrid>
 	{/if}
 </section>
+
+{#if showMetadataOverlayOpen && showMetadataShow}
+	<ShowMetadataSearchOverlay
+		show={showMetadataShow}
+		close={() => {
+			showMetadataOverlayOpen = false;
+		}}
+	/>
+{/if}
