86df7e72b822292d7e844f96aa9ae9d1abaf5c19
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-05-04 21:22:26 +0200

    percentage bar show actual percentage when debug is active

--- src/frontend/src/lib/components/ui/PercentageBar.svelte
+++ src/frontend/src/lib/components/ui/PercentageBar.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { debug } from '$lib/debug.svelte';
+
 	interface Props {
 		percentage: number;
 	}
@@ -7,4 +9,9 @@
 
 <div class="bg-neutral-600">
 	<div style={`width: ${percentage}%;`} class={`h-2 bg-brand text-sm`}></div>
+	{#if debug.showDebug}
+		<span class="text-shadow text-white text-shadow-black text-shadow-lg"
+			>{percentage.toFixed(2)}%</span
+		>
+	{/if}
 </div>
