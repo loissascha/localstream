15391421e873abaec106726a6c1531a906056d2c
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-05-04 21:18:11 +0200

    debug checkbox

--- src/frontend/src/lib/components/ui/LayoutUserDropdown.svelte
+++ src/frontend/src/lib/components/ui/LayoutUserDropdown.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import { resolve } from '$app/paths';
 	import { auth } from '$lib/auth.svelte';
+	import { debug, showDebug } from '$lib/debug.svelte';
+	import CheckIcon from '$lib/icons/CheckIcon.svelte';
 	import LogoutIcon from '$lib/icons/LogoutIcon.svelte';
 	import SettingsIcon from '$lib/icons/SettingsIcon.svelte';
 	import UserIcon from '$lib/icons/UserIcon.svelte';
+	import XIcon from '$lib/icons/XIcon.svelte';
 	import DropdownItem from './DropdownItem.svelte';
 	import DropdownMelt from './DropdownMelt.svelte';
 </script>
@@ -15,6 +18,20 @@
 			<DropdownItem href={resolve('/(protected)/(admin)/admin')}>
 				<div class="flex items-center gap-1">
 					<SettingsIcon /> Settings
+				</div>
+			</DropdownItem>
+			<DropdownItem
+				onclick={() => {
+					showDebug(!debug.showDebug);
+				}}
+			>
+				<div class="flex items-center gap-1">
+					{#if debug.showDebug}
+						<CheckIcon />
+					{:else}
+						<XIcon />
+					{/if}
+					Debug
 				</div>
 			</DropdownItem>
 		{/if}
