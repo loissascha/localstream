b7860c3aa9cef024946a6f8e3cf632413e8a2a6e
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-05-07 21:19:36 +0200

    longer time for library cataloguer to run

--- src/internal/service/background/library_cataloguer.go
+++ src/internal/service/background/library_cataloguer.go
@@ -91,7 +91,7 @@ func (l *LibraryCataloguer) RunBackground() {
 }
 
 func (l *LibraryCataloguer) RunOnce() error {
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
 	defer cancel()
 
 	libraries, err := l.libService.List(ctx)
