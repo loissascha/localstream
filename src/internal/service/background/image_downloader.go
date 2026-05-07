5e0c4ffe5d0930cd0eb53c1237c209256ddc8a57
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-05-07 19:01:34 +0200

    image downloader

---
+++ src/internal/service/background/image_downloader.go
@@ -0,0 +1,39 @@
+package background
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+func downloadImage(url string, filename string) error {
+	err := os.MkdirAll("./static", os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to download image: status %s", resp.Status)
+	}
+
+	filePath := filepath.Join("./static", filename)
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		return err
+	}
+	return nil
+}
