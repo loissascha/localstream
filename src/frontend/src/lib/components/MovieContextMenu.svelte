79b41d8bd6fcc1aa67fe356a8e95bde2d2ffe8e5
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-05-10 10:39:15 +0200

    movie context menu for set watched / not watched

--- src/frontend/src/lib/components/MovieContextMenu.svelte
+++ src/frontend/src/lib/components/MovieContextMenu.svelte
@@ -3,6 +3,9 @@
 	import type { Snippet } from 'svelte';
 	import MovieMetadataSearchOverlay from './overlays/MovieMetadataSearchOverlay.svelte';
 	import ContextMenu from './ui/ContextMenu.svelte';
+	import { deleteWatchstateMovie, setWatchstateFinishedMovie } from '$lib/api/watchstate_movie';
+	import { auth } from '$lib/auth.svelte';
+	import { reloadSingleMovie } from '$lib/movies.svelte';
 
 	interface Props {
 		children: Snippet;
@@ -16,16 +19,51 @@
 <ContextMenu closeOnItemClick={true}>
 	{@render children()}
 	{#snippet items(closeMenu)}
-		<button
-			role="menuitem"
-			onclick={(e) => {
-				e.preventDefault();
-				e.stopPropagation();
-				closeMenu();
-				movieMetadataOverlayOpen = true;
-			}}
-			class="cursor-pointer px-4 py-2 hover:bg-neutral-700">Update Metadata</button
-		>
+		{#if auth.isAdmin}
+			<button
+				role="menuitem"
+				onclick={(e) => {
+					e.preventDefault();
+					e.stopPropagation();
+					closeMenu();
+					movieMetadataOverlayOpen = true;
+				}}
+				class="cursor-pointer px-4 py-2 hover:bg-neutral-700">Update Metadata</button
+			>
+		{/if}
+		{#if movie.finished}
+			<button
+				role="menuitem"
+				onclick={(e) => {
+					e.preventDefault();
+					e.stopPropagation();
+					if (auth.token) {
+						deleteWatchstateMovie(auth.token, movie.id).then(() => {
+							// loadMoviesDatabase();
+							reloadSingleMovie(movie.id);
+						});
+					}
+					closeMenu();
+				}}
+				class="cursor-pointer px-4 py-2 hover:bg-neutral-700">Set Not Watched</button
+			>
+		{:else}
+			<button
+				role="menuitem"
+				onclick={(e) => {
+					e.preventDefault();
+					e.stopPropagation();
+					if (auth.token) {
+						setWatchstateFinishedMovie(auth.token, movie.id).then(() => {
+							// loadMoviesDatabase();
+							reloadSingleMovie(movie.id);
+						});
+					}
+					closeMenu();
+				}}
+				class="cursor-pointer px-4 py-2 hover:bg-neutral-700">Set Watched</button
+			>
+		{/if}
 	{/snippet}
 </ContextMenu>
 
