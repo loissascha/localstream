086582dc928904414932a2b03743bece0ef0777e
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-05-12 18:42:32 +0200

    search overlay

--- src/frontend/src/lib/components/overlays/MovieSubtitleSearchOverlay.svelte
+++ src/frontend/src/lib/components/overlays/MovieSubtitleSearchOverlay.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
-	import type { MovieInfo } from '$lib/types/export_types';
+	import { searchMovieSubtitles } from '$lib/api/movie_subtitles';
+	import { auth } from '$lib/auth.svelte';
+	import SearchIcon from '$lib/icons/SearchIcon.svelte';
+	import type { SubtitleProviderResult, MovieInfo } from '$lib/types/export_types';
 	import Overlay from './Overlay.svelte';
 
 	interface Props {
@@ -7,8 +10,74 @@
 		movie: MovieInfo;
 	}
 	let { close, movie }: Props = $props();
+
+	let searchingMetadata = $state(false);
+	let searchQuery = $state('');
+	let subtitleResult = $state<SubtitleProviderResult[]>([]);
+
+	$effect(() => {
+		if (!auth.initialized) return;
+		if (!auth.token) return;
+		movie;
+		searchMovieSubtitles(auth.token, movie.name)
+			.then((result) => {
+				subtitleResult = result;
+			})
+			.catch((e) => {
+				const m = (e as Error).message;
+				alert(m);
+			});
+	});
+
+	function submitSearchForm() {
+		try {
+			if (searchingMetadata) return;
+			if (!auth.token) return;
+			searchingMetadata = true;
+			if (searchQuery == '') {
+				alert('No query');
+				return;
+			}
+			searchMovieSubtitles(auth.token, searchQuery)
+				.then((result) => {
+					subtitleResult = result;
+				})
+				.catch((e) => {
+					const m = (e as Error).message;
+					alert(m);
+				})
+				.finally(() => {
+					searchingMetadata = false;
+				});
+		} catch (e) {
+			const m = (e as Error).message;
+			alert(m);
+		}
+	}
 </script>
 
 <Overlay {close}>
 	<h1 class="text-2xl font-bold tracking-wide">Subtitles {movie.name}</h1>
+	<form
+		onsubmit={(e) => {
+			e.preventDefault();
+			submitSearchForm();
+		}}
+		class="my-4 flex items-center gap-2"
+	>
+		<input
+			bind:value={searchQuery}
+			type="text"
+			class="my-4 w-full rounded bg-neutral-700 px-4 py-2"
+			placeholder="Search for new Metadata"
+		/>
+		<button class="flex cursor-pointer gap-2 rounded bg-neutral-700 px-4 py-2 hover:bg-neutral-600">
+			<SearchIcon /> Search
+		</button>
+	</form>
+	{#each subtitleResult as subtitle}
+		<div>
+			{subtitle.name}
+		</div>
+	{/each}
 </Overlay>
