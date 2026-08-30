f7e7cf0b2585bdf46ac1313fd66e541a908901c9
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-08-30 17:25:45 +0200

    update build script

--- src/build_prod.sh
+++ src/build_prod.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 cd frontend
+rm -rf build
+mkdir build
+cd ../../frontend
 pnpm install --frozen-lockfile
 pnpm run build
-cd ..
+cp -r build/* ../src/frontend/build/
+cd ../src
 go build -o localstream ./cmd/server
